0fa17a790831f704de8430bc9b53fb877ecaab8d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-03-18 18:28:55 +0000

    Vertical collisions functional

--- Assets/scripts/Controller2D.cs
+++ Assets/scripts/Controller2D.cs
@@ -5,8 +5,11 @@ public class Controller2D : MonoBehaviour {
 
     private const float skinWidth = 0.15f;
 
-    public int horizontalRayCount = 4;
-    public int verticalRayCount = 4;
+    [SerializeField]
+    private LayerMask collisionMask;
+
+    private int horizontalRayCount = 4;
+    private int verticalRayCount = 4;
     private float horizontalRaySpacing;
     private float verticalRaySpacing;
 
@@ -18,14 +21,37 @@ public class Controller2D : MonoBehaviour {
         collider = GetComponent<BoxCollider2D> ();
     }
 
-    private void Update ()
+    private void Start ()
+    {
+        CalculateRaySpacing ();
+    }
+
+    public void Move (Vector3 velocity)
     {
         UpdateRaycastOrigins ();
-        CalculateRaySpacing ();
+        VerticalCollisions (ref velocity);
+        transform.Translate (velocity);
+    }
+
+    private void VerticalCollisions (ref Vector3 velocity)
+    {
+        float directionY = Mathf.Sign (velocity.y);
+        float rayLength = Mathf.Abs (velocity.y) + skinWidth;
 
         for (int i = 0; i < verticalRayCount; i++)
         {
-            Debug.DrawRay (raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+
+            RaycastHit2D hit = Physics2D.Raycast (rayOrigin, Vector2.up * directionY, rayLength, collisionMask);
+
+            if (hit)
+            {
+                velocity.y = (hit.distance - skinWidth) * directionY;
+                rayLength = hit.distance;
+            }
+
+            Debug.DrawRay (rayOrigin, Vector2.up * directionY, Color.red);
         }
     }
 
--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,10 +4,18 @@
 public class Player : MonoBehaviour {
 
 	private Controller2D controller;
+    private float gravity = -20f;
+    private Vector3 velocity;
 
     private void Awake ()
     {
         controller = GetComponent<Controller2D> ();
+    }
+
+    private void Update ()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move (velocity * Time.deltaTime);
     }
 
 }
