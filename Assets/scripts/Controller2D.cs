6464754bb52826e6faada3fcbbd9adf52103eefb
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-03-18 18:00:02 +0000

    Calculate rays for controller

--- Assets/scripts/Controller2D.cs
+++ Assets/scripts/Controller2D.cs
@@ -1,7 +1,59 @@
 ﻿using UnityEngine;
 
+[RequireComponent (typeof (BoxCollider2D))]
 public class Controller2D : MonoBehaviour {
 
-	
+    private const float skinWidth = 0.15f;
+
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
+    private float horizontalRaySpacing;
+    private float verticalRaySpacing;
+
+	private BoxCollider2D collider;
+    private RaycastOrigins raycastOrigins;
+
+    private void Awake ()
+    {
+        collider = GetComponent<BoxCollider2D> ();
+    }
+
+    private void Update ()
+    {
+        UpdateRaycastOrigins ();
+        CalculateRaySpacing ();
+
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            Debug.DrawRay (raycastOrigins.bottomLeft + Vector2.right * verticalRaySpacing * i, Vector2.up * -2, Color.red);
+        }
+    }
+
+    private void UpdateRaycastOrigins ()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand (skinWidth * -2);
+
+        raycastOrigins.bottomLeft = new Vector2 (bounds.min.x, bounds.min.y);
+        raycastOrigins.bottomRight = new Vector2 (bounds.max.x, bounds.min.y);
+        raycastOrigins.topLeft = new Vector2 (bounds.min.x, bounds.max.y);
+        raycastOrigins.topRight = new Vector2 (bounds.max.x, bounds.max.y);
+    }
+
+    private void CalculateRaySpacing ()
+    {
+        Bounds bounds = collider.bounds;
+        bounds.Expand (skinWidth * -2);
+
+        horizontalRayCount = Mathf.Clamp (horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp (verticalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    private struct RaycastOrigins {
+        public Vector2 topLeft, topRight, bottomLeft, bottomRight;
+    }
 
 }
--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
 
+[RequireComponent (typeof (Controller2D))]
 public class Player : MonoBehaviour {
 
-	
+	private Controller2D controller;
+
+    private void Awake ()
+    {
+        controller = GetComponent<Controller2D> ();
+    }
 
 }
