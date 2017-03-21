c08d5a498d1c0e03982d04b4bb5a22a6f924ddc7
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-03-21 07:34:59 +0000

    Can climb slopes smoothly

--- Assets/scripts/Controller2D.cs
+++ Assets/scripts/Controller2D.cs
@@ -15,6 +15,8 @@ public class Controller2D : MonoBehaviour {
     private float horizontalRaySpacing;
     private float verticalRaySpacing;
 
+    private float maxClimbAngle = 60;
+
 	private BoxCollider2D collider;
     private RaycastOrigins raycastOrigins;
 
@@ -59,6 +61,13 @@ public class Controller2D : MonoBehaviour {
 
             if (hit)
             {
+                float slopeAngle = Vector2.Angle (hit.normal, Vector2.up);
+
+                if (i == 0 && slopeAngle <= maxClimbAngle)
+                {
+                    ClimbSlope (ref velocity, slopeAngle);
+                }
+
                 velocity.x = (hit.distance - skinWidth) * directionX;
                 rayLength = hit.distance;
                 collisions.left = directionX == -1;
@@ -92,6 +101,13 @@ public class Controller2D : MonoBehaviour {
 
             Debug.DrawRay (rayOrigin, Vector2.up * directionY * rayLength, Color.red);
         }
+    }
+
+    private void ClimbSlope (ref Vector3 velocity, float slopeAngle)
+    {
+        float moveDistance = Mathf.Abs (velocity.x);
+        velocity.y = Mathf.Sin (slopeAngle * Mathf.Deg2Rad) * moveDistance;
+        velocity.x = Mathf.Cos (slopeAngle * Mathf.Deg2Rad) * moveDistance * Mathf.Sign (velocity.x);
     }
 
     private void UpdateRaycastOrigins ()
